c8fe8a05c7dad81474957fab418bbd415d628aae
zLuCaS2K <JoseLucas50000@Gmail.com>
2020-03-14 15:49:41 -0300

    Implementado NavigationView

--- app/src/main/java/com/lucasprojects/sorteiointegrador/activities/MainActivity.kt
+++ app/src/main/java/com/lucasprojects/sorteiointegrador/activities/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.lucasprojects.sorteiointegrador.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.lucasprojects.sorteiointegrador.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /** Definindo a cor dos itens da NavigationView como nulas */
+        navigationView.itemIconTintList = null
+        /** Definindo o NavController */
+        val navController = Navigation.findNavController(this, R.id.navHostFragment)
+        /** Configurando a Navegação do App */
+        NavigationUI.setupWithNavController(navigationView, navController)
+        /** Execução com a navegação baseada no destino */
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.navOffline -> textMode.text = getString(R.string.off)
+                R.id.navOnline -> textMode.text = getString(R.string.on)
+            }
+        }
     }
 }

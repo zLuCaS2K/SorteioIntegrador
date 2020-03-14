b2a7343948c8ecb3831430257f84902f6bf14159
zLuCaS2K <JoseLucas50000@Gmail.com>
2020-03-14 15:21:41 -0300

    Adicionado Constants

---
+++ app/src/main/java/com/lucasprojects/sorteiointegrador/constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.lucasprojects.sorteiointegrador.constants
+
+class Constants {
+
+    object TEAMS_NAMES {
+        const val TEAMONE = "SISTEMA PETSHOP"
+        const val TEAMTWO = "SISTEMA PARA AUTONOMOS"
+        const val TEAMTHREE = "PAUSA PARA O CAFÉ"
+        const val TEAMFOUR = "PORTAL PRA DISNEY"
+        const val TEAMFIVE = "GERENCIAMENTO DE STARTUP"
+    }
+}
